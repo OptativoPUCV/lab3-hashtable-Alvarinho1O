0d1f92681d0bb293385fda7828ba8b15768180f6
Replit user <>
2023-09-07 21:16:16 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -78,12 +78,12 @@ void enlarge(HashMap * map) {
   long new_capacity = map->capacity * 2;
   Pair *new_buckets = (Pair *)calloc(new_capacity, sizeof(Pair));
 
-  if (new_buckets == NULL) {
+  if(new_buckets == NULL) {
     return;
   }
 
-  for (long i = 0; i < map->capacity; i++) {
-    Pair currentPair = map->buckets[i];
+  for(long i = 0; i < map->capacity; i++) {
+    Pair *currentPair = map->buckets[i];
     if (currentPair != NULL && currentPair->key != NULL) {
       long pos = hash(currentPair->key, new_capacity);
       while (new_buckets[pos] != NULL) {
